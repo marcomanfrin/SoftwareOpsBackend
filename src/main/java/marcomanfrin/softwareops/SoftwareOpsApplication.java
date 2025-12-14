9d9d45c053971e2b505c1ae59832e7da7726ce99
marcomanfrin <marco.manfrin04@gmail.com>
2025-12-14 14:37:36 +0100

    first commit

---
+++ src/main/java/marcomanfrin/softwareops/SoftwareOpsApplication.java
@@ -0,0 +1,13 @@
+package marcomanfrin.softwareops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/marcomanfrin/softwareops/SoftwareOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package marcomanfrin.softwareops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
