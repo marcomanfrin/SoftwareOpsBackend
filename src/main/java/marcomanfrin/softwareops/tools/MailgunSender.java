b6d117d00768c4d5dcc9fe5d3814fe9f0290ab70
marcomanfrin <marco.manfrin04@gmail.com>
2025-12-21 17:34:50 +0100

    mailgun integraion added

--- src/main/java/marcomanfrin/softwareops/tools/MailgunSender.java
+++ src/main/java/marcomanfrin/softwareops/tools/MailgunSender.java
@@ -11,26 +11,24 @@ import org.springframework.stereotype.Component;
 public class MailgunSender {
     private final String domainName;
     private final String apiKey;
-    private String sender;
+    private final String sender;
 
     public MailgunSender(@Value("${mailgun.domain}") String domainName,
-                         @Value("${mailgun.apiKey}") String apiKey/*,
-                         @Value("${mailgun.sender}") String sender*/) {
+                         @Value("${mailgun.apiKey}") String apiKey,
+                         @Value("${mailgun.sender}") String sender) {
         this.domainName = domainName;
         this.apiKey = apiKey;
+        this.sender = sender;
     }
 
     public void sendRegistrationEmail(User recipient) {
         HttpResponse<JsonNode> response = Unirest.post("https://api.mailgun.net/v3/" + this.domainName + "/messages")
                 .basicAuth("api", this.apiKey)
-                .queryString("from", "marco.manfrin@gmail.com"/*this.sender*/)
+                .queryString("from", this.sender)
                 .queryString("to", recipient.getEmail())
                 .queryString("subject", "Benvenuto sulla piattaforma")
                 .queryString("text", "Ciao, " + recipient.getFirstName() + ", la registrazione è avvenuta con successo!")
                 .asJson();
-
-        // TODO: remove
-        System.out.println("AAA" + response.getBody()); // <-- Consiglio questo log per debuggare eventuali problemi
     }
 
 
