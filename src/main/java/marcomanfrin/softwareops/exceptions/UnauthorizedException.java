981175c2860b8b4e677f08e1e1c2590d7aa5293a
marcomanfrin <marco.manfrin04@gmail.com>
2025-12-22 10:48:50 +0100

    some custom exception added

---
+++ src/main/java/marcomanfrin/softwareops/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package marcomanfrin.softwareops.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+}
